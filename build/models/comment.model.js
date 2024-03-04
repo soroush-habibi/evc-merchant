4a63c5b661b94315fdf7bd02caa2e324d34bc8c6
Soroush Habibi <soroush.habibi.1382@gmail.com>
2024-03-04 14:24:59 +0330

    implement comment model

---
+++ build/models/comment.model.js
@@ -0,0 +1,68 @@
+import mongoose, { model, Schema } from 'mongoose';
+const commentSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    inventoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Inventory",
+        required: true
+    },
+    orderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    context: {
+        type: String,
+        required: true
+    },
+    pros: {
+        type: [String],
+        default: []
+    },
+    cons: {
+        type: [String],
+        default: []
+    },
+    rate: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    suggest: {
+        type: Boolean,
+        required: true
+    },
+    like: {
+        type: Number,
+        default: 0
+    },
+    dislike: {
+        type: Number,
+        default: 0
+    }
+}, {
+    toJSON: {
+        // convert _id to id
+        virtuals: true,
+        // remove __v
+        versionKey: false,
+        // remove _id 
+        transform: function (doc, ret) {
+            delete ret._id;
+            // delete ret.__v;
+        }
+    },
+    timestamps: true
+});
+commentSchema.index({ userId: 1 });
+commentSchema.index({ inventoryId: 1 });
+const Comment = model('Comment', commentSchema);
+export { Comment };
---
+++ source/models/comment.model.ts
@@ -0,0 +1,92 @@
+import mongoose, { Model, model, Schema } from 'mongoose';
+import { orderStatusEnum } from "../enum/orderStatus.enum.js";
+import validator from 'validator';
+
+export interface IComment {
+    userId: mongoose.Schema.Types.ObjectId,
+    inventoryId: mongoose.Schema.Types.ObjectId,
+    orderId: mongoose.Schema.Types.ObjectId,
+    title: string,
+    context: string,
+    pros: string[],
+    cons: string[],
+    rate: number,
+    suggest: boolean,
+    like: number,
+    dislike: number
+}
+
+export interface ICommentMethods { }
+
+type CommentModel = Model<IComment, {}, ICommentMethods>;
+
+const commentSchema = new Schema<IComment, CommentModel, ICommentMethods>({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    inventoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Inventory",
+        required: true
+    },
+    orderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    context: {
+        type: String,
+        required: true
+    },
+    pros: {
+        type: [String],
+        default: []
+    },
+    cons: {
+        type: [String],
+        default: []
+    },
+    rate: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    suggest: {
+        type: Boolean,
+        required: true
+    },
+    like: {
+        type: Number,
+        default: 0
+    },
+    dislike: {
+        type: Number,
+        default: 0
+    }
+}, {
+    toJSON: {
+        // convert _id to id
+        virtuals: true,
+        // remove __v
+        versionKey: false,
+        // remove _id 
+        transform: function (doc, ret) {
+            delete ret._id;
+            // delete ret.__v;
+        }
+    },
+    timestamps: true
+});
+
+commentSchema.index({ userId: 1 });
+commentSchema.index({ inventoryId: 1 });
+
+const Comment = model<IComment, CommentModel>('Comment', commentSchema);
+
+export { Comment };
